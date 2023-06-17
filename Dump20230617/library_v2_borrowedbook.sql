2864444e51f796219195425c92cc27e65463d147
Felbertyarte <131159437+Felbertyarte@users.noreply.github.com>
2023-06-17 11:07:56 +0800

    Add files via upload

---
+++ Dump20230617/library_v2_borrowedbook.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: library_v2
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `borrowedbook`
+--
+
+DROP TABLE IF EXISTS `borrowedbook`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `borrowedbook` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `bookID` int NOT NULL,
+  `borrowerID` int NOT NULL,
+  `issueDate` date DEFAULT NULL,
+  `returndate` date DEFAULT NULL,
+  `daysLate` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `bookID` (`bookID`),
+  KEY `borrowerID` (`borrowerID`),
+  CONSTRAINT `borrowedbook_ibfk_1` FOREIGN KEY (`bookID`) REFERENCES `book` (`BID`),
+  CONSTRAINT `borrowedbook_ibfk_2` FOREIGN KEY (`borrowerID`) REFERENCES `borrower` (`borrower_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `borrowedbook`
+--
+
+LOCK TABLES `borrowedbook` WRITE;
+/*!40000 ALTER TABLE `borrowedbook` DISABLE KEYS */;
+INSERT INTO `borrowedbook` VALUES (6,1234,113003,'2023-06-17','2023-06-20',7);
+/*!40000 ALTER TABLE `borrowedbook` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-06-17 11:03:26
